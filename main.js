8e42e13c33f80f1a7dd906102d00ed0e9a32768b
kilicay <osmankilicayy@gmail.com>
2023-08-30 16:15:39 +0300

    test commit

--- main.js
+++ main.js
@@ -1,13 +1,57 @@
-const { app, BrowserWindow, ipcMain, Tray, Menu, autoUpdater } = require("electron");
+const { app, BrowserWindow, ipcMain, Tray, Menu,dialog,autoUpdater } = require("electron");
 const fetch = require("isomorphic-fetch");
 const { generatePrintContent } = require("./print-utils");
 const Store = require("electron-store");
 const store = new Store();
 const path = require("path");
+const axios = require("axios");
+const appVersion = app.getVersion();
 
 let mainWindow;
 let printerId;
 let tray = null;
+
+const owner = "KILICAY";
+const repo = "butiksoftpos";
+const apiUrl = `https://api.github.com/repos/${owner}/${repo}/releases/latest`;
+// autoUpdater.setFeedURL(apiUrl)
+
+const updateURL = 'https://github.com/KILICAY/butiksoftpos/releases';
+autoUpdater.setFeedURL({
+  url: updateURL,
+});
+axios.get(apiUrl)
+  .then(response => {
+    const latestVersion = response.data.tag_name;
+    if (latestVersion !== appVersion) {
+      const dialogOptions = {
+        type: "question",
+        buttons: ["Şimdi Güncelle", "Daha Sonra"],
+        defaultId: 0,
+        title: "Yeni Güncelleme",
+        message: `Mevcut versiyon: ${appVersion}\nYeni versiyon: ${latestVersion}\nUygulamayı güncellemek ister misiniz?`,
+      };
+
+      dialog.showMessageBox(null, dialogOptions).then((result) => {
+        if (result.response === 0) {
+          // Kullanıcı "Şimdi Güncelle" seçeneğini tıkladı
+          console.log("Kullanici guncelleme yapmayi secti.");
+          autoUpdater.checkForUpdates() 
+          autoUpdater.quitAndInstall()
+          // Burada güncelleme işlemlerini gerçekleştirin
+        } else {
+          // Kullanıcı "Daha Sonra" seçeneğini tıkladı
+          console.log("Kullanici guncelleme yapmayi secmedi.");
+        }
+      });
+    } else {
+      console.log("Uygulama güncel.");
+    }
+  })
+  .catch(error => {
+    console.error("Güncelleme kontrolünde hata:", error.message);
+  });
+
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -17,18 +61,21 @@ function createWindow() {
       nodeIntegration: true,
       contextIsolation: false,
     },
-    show: false,
+    show: false, // Hide the window initially
   });
   mainWindow.loadFile("index.html");
 
+  // Handle window close event
   mainWindow.on("close", (event) => {
+    // Hide the window instead of quitting the app
     event.preventDefault();
     mainWindow.hide();
   });
 }
 
-app.requestSingleInstanceLock();
+app.requestSingleInstanceLock(); // Check if this is the first instance
 app.on("second-instance", () => {
+  // If a second instance is launched, focus the existing window
   if (mainWindow) {
     if (mainWindow.isMinimized()) mainWindow.restore();
     mainWindow.focus();
@@ -36,6 +83,7 @@ app.on("second-instance", () => {
 });
 
 app.whenReady().then(() => {
+  // Create the tray icon
   tray = new Tray(path.join(__dirname, "tray-icon.png"));
   const contextMenu = Menu.buildFromTemplate([
     {
@@ -44,33 +92,14 @@ app.whenReady().then(() => {
     },
     {
       label: "Kapat",
-      click: quitApp,
+      click: quitApp, // quitApp fonksiyonunu çağırarak uygulamayı kapatıyoruz
     },
   ]);
   tray.setToolTip("Print Server");
   tray.setContextMenu(contextMenu);
   Menu.setApplicationMenu(null);
   createWindow();
-  initAutoUpdater();
 });
-
-function initAutoUpdater() {
-  autoUpdater.setFeedURL({
-    provider: "github",
-    owner: "KILICAY",
-    repo: "ButiksoftPOS-PrintServer",
-  });
-
-  autoUpdater.checkForUpdatesAndNotify();
-
-  autoUpdater.on("download-progress", (progressObj) => {
-    mainWindow.webContents.send("update-download-progress", progressObj.percent);
-  });
-
-  autoUpdater.on("update-downloaded", (info) => {
-    mainWindow.webContents.send("update-downloaded", info);
-  });
-}
 
 printerId = store.get("printerId");
 ipcMain.on("getDataAndPrint", async (event, arg) => {
@@ -125,6 +154,7 @@ app.on("browser-window-created", (event, window) => {
   });
 });
 
+// Kapat seçeneğine tıklanınca uygulamayı kapat
 function quitApp() {
   app.exit();
 }
